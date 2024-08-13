567b0f93ec4c86b976871565663feb75654c9a34
yanfi-sher <loss.soul20@gmail.com>
2024-08-14 01:45:57 +0300

    HW 17 (static day of week)

--- src/main/java/telran/time/Main.java
+++ src/main/java/telran/time/Main.java
@@ -1,6 +1,7 @@
 package telran.time;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 record MonthYear(int month, int year){
 }
@@ -27,35 +28,59 @@ public class Main {
     }
 
     private static MonthYear getMonthYear(String[] args) throws Exception {
-        return null;
+        if (args.length != 2) {
+            throw new Exception("Please provide month and year as arguments.");
+        }
+        try {
+            int month = Integer.parseInt(args[0]);
+            int year = Integer.parseInt(args[1]);
+            if (month < 1 || month > 12) {
+                throw new Exception("Month must be between 1 and 12.");
+            }
+            return new MonthYear(month, year);
+        } catch (NumberFormatException e) {
+            throw new Exception("Invalid number format for month or year.");
+        }
     }
 
     private static void printTitle(MonthYear monthYear) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String monthName = Month.of(monthYear.month()).name();
+        System.out.println("    "+ monthName + " " + monthYear.year());
     }
 
     private static void printWeekDays() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Mo Tu We Th Fr Sa Su");
     }
 
     private static void printDates(MonthYear monthYear) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int firstDayOfWeek = getFirstDayOfWeek(monthYear);
+        int lastDayOfMonth = getLastDayOfMonth(monthYear);
+        int offset = getOffSet(firstDayOfWeek);
+
+        for (int i = 0; i < offset; i++) {
+            System.out.print("   ");
+        }
+
+        for (int day = 1; day <= lastDayOfMonth; day++) {
+            System.out.printf("%2d ", day);
+            if ((offset + day) % 7 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
     }
 
-    private int getFirstDayOfWeek(MonthYear monthYear){
+    private static int getFirstDayOfWeek(MonthYear monthYear){
         LocalDate firstDay = LocalDate.of(monthYear.year(), monthYear.month(), 1);
         return firstDay.getDayOfWeek().getValue();
     }
 
-    private int getOffSet(int firstWeekDay){
-        return firstWeekDay % 7;
+    private static int getOffSet(int firstWeekDay){
+        return firstWeekDay-1 % 7;
     }
 
     private static int getLastDayOfMonth(MonthYear monthYear) {
-        LocalDate lastDay = LocalDate.of(monthYear.year(), monthYear.month(), 1)
-                                      .withDayOfMonth(1)
-                                      .plusMonths(1)
-                                      .minusDays(1);
+        LocalDate lastDay = LocalDate.of(monthYear.year(), monthYear.month(), 1).withDayOfMonth(1).plusMonths(1).minusDays(1);
         return lastDay.getDayOfMonth();
     }
 }
